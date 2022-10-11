4bf7ac808081320404706e078356ffc85b14e449
Zachary Snoek <zacharysnoek@Zacharys-MacBook-Pro-2.local>
2022-10-10 19:47:00 -0700

    exercise 2: text truncation

--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -7,7 +7,10 @@ const SecondaryStory = ({ id, title, image, location, abstract }) => {
       <Wrapper>
         <Image alt={image.alt} src={image.src} />
         <Heading>{title}</Heading>
+        <AbstractWrapper>
+
         <Abstract>{abstract}</Abstract>
+        </AbstractWrapper>
       </Wrapper>
     </a>
   );
@@ -41,10 +44,19 @@ const Heading = styled.h2`
   margin-top: -2px;
 `;
 
+const AbstractWrapper = styled.div`
+  grid-area: abstract;
+`;
+
 const Abstract = styled.p`
   grid-area: abstract;
   font-size: 1rem;
   white-space: pre-wrap;
+
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
 `;
 
 export default SecondaryStory;
