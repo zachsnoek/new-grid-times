321e1800586ef81344977f254aa97e088535d396
zachsnoek <dev@zachsnoek.com>
2022-10-12 20:23:42 -0700

    complete exercise 3

--- src/components/MainStoryGrid/MainStoryGrid.js
+++ src/components/MainStoryGrid/MainStoryGrid.js
@@ -1,92 +1,148 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import {
-  MAIN_STORY,
-  OPINION_STORIES,
-  SECONDARY_STORIES,
-} from '../../data';
+import { MAIN_STORY, OPINION_STORIES, SECONDARY_STORIES } from '../../data';
 
 import SectionTitle from '../SectionTitle';
 import MainStory from '../MainStory';
 import SecondaryStory from '../SecondaryStory';
 import OpinionStory from '../OpinionStory';
 import Advertisement from '../Advertisement';
+import { QUERIES } from '../../constants';
 
 const MainStoryGrid = () => {
-  return (
-    <Wrapper>
-      <MainStorySection>
-        <MainStory {...MAIN_STORY} />
-      </MainStorySection>
+    return (
+        <Wrapper>
+            <MainStorySection>
+                <MainStory {...MAIN_STORY} />
+            </MainStorySection>
 
-      <SecondaryStorySection>
-        <StoryList>
-          {SECONDARY_STORIES.map((story, index) => (
-            <VerticalStoryWrapper key={story.id}>
+            <SecondaryStorySection>
+                <StoryList>
+                    {SECONDARY_STORIES.map((story, index) => (
+                        <VerticalStoryWrapper key={story.id}>
+                            <SecondaryStory {...story} />
+                        </VerticalStoryWrapper>
+                    ))}
+                </StoryList>
+            </SecondaryStorySection>
 
-            <SecondaryStory  {...story} />
-            </VerticalStoryWrapper>
-          ))}
-        </StoryList>
-      </SecondaryStorySection>
+            <OpinionSection>
+                <SectionTitle>Opinion</SectionTitle>
+                <OpinionStoryList>
+                    {OPINION_STORIES.map((story, index) => (
+                        <OpinionStoryWrapper key={story.id}>
+                            <OpinionStory {...story} />
+                        </OpinionStoryWrapper>
+                    ))}
+                </OpinionStoryList>
+            </OpinionSection>
 
-      <OpinionSection>
-        <SectionTitle>Opinion</SectionTitle>
-        <StoryList>
-          {OPINION_STORIES.map((story, index) => (
-            <VerticalStoryWrapper key={story.id} >
-              <OpinionStory {...story} />
-            </VerticalStoryWrapper>
-          ))}
-        </StoryList>
-      </OpinionSection>
-
-      <AdvertisementSection>
-        <Advertisement />
-      </AdvertisementSection>
-    </Wrapper>
-  );
+            <AdvertisementSection>
+                <Advertisement />
+            </AdvertisementSection>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
-  display: grid;
-  grid-template-areas:
-    'main-story'
-    'secondary-stories'
-    'opinion-stories'
-    'advertisement';
-  gap: 48px;
-  margin-bottom: 48px;
+    display: grid;
+    grid-template-areas:
+        'main-story'
+        'secondary-stories'
+        'opinion-stories'
+        'advertisement';
+    gap: 48px;
+    margin-bottom: 48px;
+
+    @media ${QUERIES.tabletAndUp} {
+        grid-template-areas:
+            'main-story secondary-stories'
+            'advertisement advertisement'
+            'opinion-stories opinion-stories';
+        grid-template-columns: 2fr 1fr;
+
+        gap: 48px 0px;
+    }
+
+    @media ${QUERIES.laptopAndUp} {
+        grid-template-areas:
+            'main-story secondary-stories opinion-stories'
+            'main-story advertisement advertisement';
+        grid-template-columns: 5fr 4fr 3fr; /* each column gets successively narrower */
+
+        gap: 0px;
+    }
 `;
 
 const MainStorySection = styled.section`
-  grid-area: main-story;
+    grid-area: main-story;
+
+    @media ${QUERIES.tabletAndUp} {
+        padding-right: 16px;
+        margin-right: 16px;
+        border-right: 1px solid var(--color-gray-300);
+    }
 `;
 
 const SecondaryStorySection = styled.section`
-  grid-area: secondary-stories;
+    grid-area: secondary-stories;
+
+    @media ${QUERIES.laptopAndUp} {
+        padding-right: 16px;
+        margin-right: 16px;
+        border-right: 1px solid var(--color-gray-300);
+    }
 `;
 
 const StoryList = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
+`;
+
+const OpinionStoryList = styled(StoryList)`
+    @media ${QUERIES.tabletOnly} {
+        flex-direction: row;
+        gap: 32px;
+    }
 `;
 
 const VerticalStoryWrapper = styled.div`
-  &:not(:last-of-type) {
-    border-bottom: 1px solid var(--color-gray-300);
-    padding-bottom: 16px;
-    margin-bottom: 16px;
-  }
+    &:not(:last-of-type) {
+        border-bottom: 1px solid var(--color-gray-300);
+        padding-bottom: 16px;
+        margin-bottom: 16px;
+    }
+`;
+
+const OpinionStoryWrapper = styled(VerticalStoryWrapper)`
+    @media ${QUERIES.tabletOnly} {
+        flex: 1;
+
+        &:not(:last-of-type) {
+            border-bottom: revert;
+            padding-bottom: revert;
+            margin-bottom: revert;
+        }
+    }
 `;
 
 const OpinionSection = styled.section`
-  grid-area: opinion-stories;
+    grid-area: opinion-stories;
+
+    @media ${QUERIES.laptopAndUp} {
+        margin-top: -8px; /* optical alignment */
+    }
 `;
 
 const AdvertisementSection = styled.section`
-  grid-area: advertisement;
+    grid-area: advertisement;
+
+    @media ${QUERIES.laptopAndUp} {
+        padding-top: 16px;
+        margin-top: 16px;
+        border-top: 1px solid var(--color-gray-300);
+    }
 `;
 
 export default MainStoryGrid;
--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -1,62 +1,70 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 
 const SecondaryStory = ({ id, title, image, location, abstract }) => {
-  return (
-    <a href={`/story/${id}`}>
-      <Wrapper>
-        <Image alt={image.alt} src={image.src} />
-        <Heading>{title}</Heading>
-        <AbstractWrapper>
-
-        <Abstract>{abstract}</Abstract>
-        </AbstractWrapper>
-      </Wrapper>
-    </a>
-  );
+    return (
+        <a href={`/story/${id}`}>
+            <Wrapper>
+                <Image alt={image.alt} src={image.src} />
+                <Heading>{title}</Heading>
+                <AbstractWrapper>
+                    <Abstract>{abstract}</Abstract>
+                </AbstractWrapper>
+            </Wrapper>
+        </a>
+    );
 };
 
 const Wrapper = styled.article`
-  display: grid;
-  grid-template-areas:
-    'image heading'
-    'image abstract';
-  gap: 4px 16px;
-  grid-template-columns: 120px 1fr;
-  color: var(--color-gray-900);
+    display: grid;
+    grid-template-areas:
+        'image heading'
+        'image abstract';
+    gap: 4px 16px;
+    grid-template-columns: 120px 1fr;
+    color: var(--color-gray-900);
+
+    @media ${QUERIES.tabletOnly} {
+        grid-template-areas:
+            'image'
+            'heading'
+            'abstract';
+        grid-template-columns: 1fr;
+    }
 `;
 
 const Image = styled.img`
-  grid-area: image;
-  display: block;
-  width: 100%;
-  height: 120px;
-  border-radius: 4px;
-  object-fit: cover;
+    grid-area: image;
+    display: block;
+    width: 100%;
+    height: 120px;
+    border-radius: 4px;
+    object-fit: cover;
 `;
 
 const Heading = styled.h2`
-  grid-area: heading;
-  font-size: 1.125rem;
-  font-weight: var(--font-weight-bold);
-  line-height: 1.3;
-  /* Optical alignment */
-  margin-top: -2px;
+    grid-area: heading;
+    font-size: 1.125rem;
+    font-weight: var(--font-weight-bold);
+    line-height: 1.3;
+    /* Optical alignment */
+    margin-top: -2px;
 `;
 
 const AbstractWrapper = styled.div`
-  grid-area: abstract;
+    grid-area: abstract;
 `;
 
 const Abstract = styled.p`
-  grid-area: abstract;
-  font-size: 1rem;
-  white-space: pre-wrap;
+    grid-area: abstract;
+    font-size: 1rem;
+    white-space: pre-wrap;
 
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
 `;
 
 export default SecondaryStory;
